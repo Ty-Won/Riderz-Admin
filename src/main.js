5cc66ce8e40d8738841f18ec60d4e78f8bc1e98c
Tyrone Wong <tyronewong96@gmail.com>
2018-11-14 14:51:42 -0500

    Navbar example

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
+import router from 'vue-router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.use(BootstrapVue)
+Vue.use(router)
 
 new Vue({
   el: '#app',

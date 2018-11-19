ad755c6b1cd235c6b8126d52fd67ca51d73fc221
RyanServera <ryans987531@hotmail.com>
2018-11-19 17:29:12 -0500

    Added a login form

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+    <div class="container">
+        <b-jumbotron header="Login" >
         <b-form @submit="onSubmit" @reset="onReset" v-if="show">
             <b-form-group id="exampleInputGroup1"
                           label="Email address:"
-                          label-for="exampleInput1"
-                          description="We'll never share your email with anyone else.">
+                          label-for="exampleInput1">
                 <b-form-input id="exampleInput1"
                               type="email"
                               v-model="form.email"
@@ -13,33 +13,19 @@
                 </b-form-input>
             </b-form-group>
             <b-form-group id="exampleInputGroup2"
-                          label="Your Name:"
+                          label="Password:"
                           label-for="exampleInput2">
                 <b-form-input id="exampleInput2"
-                              type="text"
-                              v-model="form.name"
+                              type="password"
+                              v-model="form.password"
                               required
-                              placeholder="Enter name">
+                              placeholder="Enter Password">
                 </b-form-input>
-            </b-form-group>
-            <b-form-group id="exampleInputGroup3"
-                          label="Food:"
-                          label-for="exampleInput3">
-                <b-form-select id="exampleInput3"
-                               :options="foods"
-                               required
-                               v-model="form.food">
-                </b-form-select>
-            </b-form-group>
-            <b-form-group id="exampleGroup4">
-                <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
-                    <b-form-checkbox value="me">Check me out</b-form-checkbox>
-                    <b-form-checkbox value="that">Check that out</b-form-checkbox>
-                </b-form-checkbox-group>
             </b-form-group>
             <b-button type="submit" variant="primary">Submit</b-button>
             <b-button type="reset" variant="danger">Reset</b-button>
         </b-form>
+        </b-jumbotron>
     </div>
 </template>
 
@@ -79,3 +65,11 @@
         }
     }
 </script>
+
+<style>
+.container{
+    margin-top: 5%;
+    max-width: 720px;
+    text-align: left;
+}
+</style>

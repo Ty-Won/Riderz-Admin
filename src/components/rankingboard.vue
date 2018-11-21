3fcbd2257a01cd2ca118603ee62a0caf50e38cfa
He Qian Wang <heqianw@gmail.com>
2018-11-21 04:33:46 -0500

    Added table and fixed the filter function

--- src/components/rankingboard.vue
+++ src/components/rankingboard.vue
@@ -1,10 +1,20 @@
 <template>
     <div id="rankingboard">
         <h1>{{ msg }}</h1>
+        <div id="time">
+            <form>
+                Start date:<br>
+                <input id='startTimeStamp' type="date" name="startName" value="1600-01-01"><br>
+                End date:<br>
+                <input id='endTimeStamp' type="date" name="endDame" value="2400-01-01"> <br><br>
+                <b-button @click="filterByDateWrapper()"> Submit parameters </b-button>
+            </form>
+        </div>
 		<h2>{{ driverTitle }}</h2>
 		<div id="driverPerformance"></div>
 		<h2>{{ userTitle }}</h2>
 		<div id="userPerformance"></div>
+        <br>
     </div>
 </template>
 
@@ -20,33 +30,74 @@
 				userTitle: 'User Performance'
             }
         }, 
+        methods:{
+            filterByDateWrapper(){
+                fetchPerformance();    
+            }
+        },
 		mounted () {
-			fetchPerformance()
+			// fetchPerformance()
 		}
     }
 	
 	function fetchPerformance() {
-		axios
+
+        var timeStart = document.getElementById('startTimeStamp').value;
+        var timeEnd = document.getElementById('endTimeStamp').value;
+        
+        if(timeStart == '' || timeEnd == ''){
+            timeStart = '1600-01-01';
+            timeEnd = '2400-01-01';
+        }
+        timeStart += ' 00:00:00';
+        timeEnd += ' 00:00:00';
+
+        axios
 			.get('https://riderz-t10.herokuapp.com/users/getDriverPerformance',
-				 {params: {startingTime:'1600-01-01 00:00:00', endingTime:'2400-01-01 00:00:00'}})
+				 {params: {startingTime: timeStart, endingTime: timeEnd}})
 			.then((response) => {
-				// Await until promise is fulfilled
-				var data = response.data
+                // Await until promise is fulfilled
+                var string = '<table cellpadding = 10 border = 1 align = "center" > <tr> <td> <b> Operator </b> </td> <td> <b> Number of Trips Completed </b> </td> </tr> ';
+                var data = response.data
+                var count = 0;
 				data.forEach(function(element) {
-					document.getElementById('driverPerformance').innerHTML += 
-						element['username'] + ' ' + element['tripCount'] + '<br>'
-				});
+
+					// document.getElementById('driverPerformance').innerHTML += 
+                    // 	element['username'] + ' ' + element['tripCount'] + '<br>'
+                    count++;
+                    if(count % 2 == 0){
+                        string = string + '<tr> <td>' + element['username'] + '</td> <td>' + element['tripCount'] + '</td> </tr>'
+                    }
+                    else{
+                        string = string + '<tr> <td bgcolor = "#d9d9d9">' + element['username'] + '</td> <td bgcolor = "#d9d9d9">' + element['tripCount'] + '</td> </tr>'
+                    }
+
+                });
+                string = string + '</table>';
+                document.getElementById('driverPerformance').innerHTML = string;
+                
 	
 				// Fetch user performance after driver performance
 				axios
 					.get('https://riderz-t10.herokuapp.com/users/getUserPerformance',
-						 {params: {startingTime:'1600-01-01 00:00:00', endingTime:'2400-01-01 00:00:00'}})
+				        {params: {startingTime: timeStart, endingTime: timeEnd}})
 					.then((response) => {
-						var data = response.data
+                        var string = '<table cellpadding = 10 border = 1 align = "center" > <tr> <td> <b> UserName </b> </td> <td> <b> Number of Trips Completed </b> </td> </tr> ';
+                        var data = response.data
+                        var count = 0;
 						data.forEach(function(element) {
-							document.getElementById('userPerformance').innerHTML += 
-								element['username'] + ' ' + element['tripCount'] + '<br>'
-						});
+							// document.getElementById('userPerformance').innerHTML += 
+                            // 	element['username'] + ' ' + element['tripCount'] + '<br>'
+                            count++;
+                            if(count % 2 == 0){
+                                string = string + '<tr> <td>' + element['username'] + '</td> <td>' + element['tripCount'] + '</td> </tr>'
+                            }
+                            else{
+                                string = string + '<tr> <td bgcolor = "#d9d9d9">' + element['username'] + '</td> <td bgcolor = "#d9d9d9">' + element['tripCount'] + '</td> </tr>'
+                            }
+                        });
+                        string = string + '</table>';
+                        document.getElementById('userPerformance').innerHTML = string;
 					})
 					.catch((error) => {
 						// Handle any errors here

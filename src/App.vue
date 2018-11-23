940b0a11161bdab8d359b2fe47075a178db1c6c8
Tyrone Wong <tyronewong96@gmail.com>
2018-11-22 20:56:43 -0500

    web-g10: Fixed component imports and modal extra buttons
    - Imports along with data types are functional
    - removed the extra modal buttons

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,86 @@
 
     <!--if navbar is not signed in, no profile button-->
    <navbar signedIn=False></navbar>
-   <jumbotron headerText="Riderz" leadText="Ride Sharing App Admin"></jumbotron>
+
+   <b-container>
+
+    <b-jumbotron header="Riderz" lead="Ride Sharing App">
+      <p>Ride Sharing Admin</p>
+      <signInModal align-v="center" v-bind:title="modal_title" v-bind:modalButtonTitle="modalButtonTitle" >
+      </signInModal>
+    </b-jumbotron>
+
+    <!-- <bModal id="signInModal" centered align-v="center" title="hello">
+    <div>
+      <b-form @submit="login"> 
+
+            <b-form-group   id="usernameInputGroup"
+                            label="Username">
+
+                <b-form-input id="usernameInput"
+                                type="text"
+                                v-model="form.username"
+                                required 
+                                placeholder="Enter Username"
+                                ref="signInUser">
+                </b-form-input>
+            </b-form-group>   
+
+
+            <b-form-group>
+                <b-form-input id="password"
+                                type="password"
+                                v-model="form.password"
+                                label="Password"
+                                required 
+                                placeholder="Password"
+                                ref="signInPass">
+                </b-form-input>
+            </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+
+      </b-form>
+    </div>
+    
+    </bModal> -->
+
+
+
+   </b-container>
+   
 
   </div>
   
 </template>
 
 <script>
-import navbar from './components/navbar.vue'
-import jumbotron from './components/jumbotron.vue'
+import navbar from "./components/navbar.vue";
+import signInModal from "./components/modal.vue";
+import signForm from "./components/form.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+       form: {
+       username: "",
+       password: ""
+       },
+        modal_title:"Sign In",
+      modalButtonTitle:"Sign In"
+    };
+  },
   components: {
     navbar,
-    jumbotron
-  }
-}
+    signInModal,
+    signForm
 
+  }
+};
 </script>
 
 <style>
-
 </style>
--- src/components/modal.vue
+++ src/components/modal.vue
@@ -1,7 +1,79 @@
 <template>
     <div>
-        <b-modal id="modal_component_container" title="">
-            <div id="component_container"></div>
+
+        <b-btn v-b-modal.modal_component_container>{{modalButtonTitle}}</b-btn>
+
+        <b-modal id="modal_component_container" v-bind:title="modal_title">
+                <div>
+        <b-form @submit="login"> 
+
+            <b-form-group   id="usernameInputGroup"
+                            label="Username">
+
+                <b-form-input id="usernameInput"
+                                type="text"
+                                v-model="form.username"
+                                required 
+                                placeholder="Enter Username">
+                </b-form-input>
+            </b-form-group>   
+
+
+            <b-form-group id="passwordInputGroup"
+                            label="Password">
+                <b-form-input id="password"
+                                type="password"
+                                v-model="form.password"
+                                required 
+                                placeholder="Password">
+                </b-form-input>
+            </b-form-group>
+
+
+                    <b-button type="submit" variant="primary">Submit</b-button>
+                    <b-button type="reset" variant="danger">Reset</b-button>
+
+
+
+                </b-form>
+            </div>
+            <div slot="modal-footer"></div>
         </b-modal>
     </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "signInModal",
+  props: {
+    modal_title: String,
+    modalButtonTitle: String
+  },
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login(evt) {
+      evt.preventDefault();
+      axios
+        .post("https://riderz-t10.herokuapp.com/login", {
+          username: "Marlon",
+          password: "Bernardes"
+        })
+        .then(response => console.log(response))
+        .catch(e => {
+          console.log("LOL");
+        });
+    }
+  }
+};
+</script>
+
+<style>
